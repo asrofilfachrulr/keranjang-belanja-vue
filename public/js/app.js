3e4ddec128c4b3fcb2b97dd60ba01ee98287c487
anya <riidloa@gmail.com>
2023-02-04 16:27:55 +0700

    not-allowed cursor on disabled button

--- public/js/app.js
+++ public/js/app.js
@@ -5462,11 +5462,11 @@ __webpack_require__.r(__webpack_exports__);
   data: function data() {
     return {
       btnTypeDel: {
-        text: "Delete",
+        text: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg>',
         type: "danger"
       },
       btnTypeDelOne: {
-        text: 'Reduce',
+        text: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-dash" viewBox="0 0 16 16"><path d="M6.5 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z"/><path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>' + "&nbsp;&nbsp;Reduce",
         type: "outline-danger"
       }
     };
@@ -5629,12 +5629,12 @@ __webpack_require__.r(__webpack_exports__);
   data: function data() {
     return {
       btnTypeCheckout: {
-        text: "Checkout",
+        text: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>' + "&emsp;Checkout",
         type: "success"
       },
       btnTypeClearAll: {
         text: "Clear All",
-        type: "danger"
+        type: "outline-danger"
       },
       checkoutComponentBody: _CheckoutModalContent_vue__WEBPACK_IMPORTED_MODULE_0__["default"],
       checkoutComponentFooter: _CheckoutModalFooter_vue__WEBPACK_IMPORTED_MODULE_1__["default"],
@@ -5809,7 +5809,7 @@ __webpack_require__.r(__webpack_exports__);
     return {
       bodyData: {},
       btnType: {
-        text: "Add to cart",
+        text: '<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16"><path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>' + "&emsp;Add",
         type: "primary"
       },
       btnTypeModalFooter: {
@@ -6013,7 +6013,8 @@ var render = function render() {
     }
   }), _vm._v(" "), _c("button-component", {
     attrs: {
-      btnType: _vm.btnTypeDel
+      btnType: _vm.btnTypeDel,
+      id: "delete-item"
     },
     on: {
       click: _vm.delHandleClick
@@ -6217,7 +6218,10 @@ var render = function render() {
   }, [_vm._v("Rp. " + _vm._s(_vm.total))]), _vm._v(" "), _c("span", {
     staticClass: "col-3"
   })]), _vm._v(" "), _c("div", {
-    staticClass: "row row-no-border mt-3"
+    staticClass: "row row-no-border mt-3",
+    staticStyle: {
+      "z-index": "-999"
+    }
   }, [_c("button-component", {
     attrs: {
       btnType: _vm.btnTypeCheckout,
@@ -6230,6 +6234,7 @@ var render = function render() {
     }
   }), _vm._v(" "), _c("button-component", {
     staticClass: "ml-3",
+    "class": "border-none",
     attrs: {
       btnType: _vm.btnTypeClearAll,
       disabled: Object.keys(this.cartItems) <= 0
--- resources/js/components/ListCartComponent.vue
+++ resources/js/components/ListCartComponent.vue
@@ -23,7 +23,7 @@
             <span class="col-3">Rp. {{ total }}</span>
             <span class="col-3"></span>
         </div>
-        <div class="row row-no-border mt-3">
+        <div class="row row-no-border mt-3" style="z-index: -999">
             <button-component
                 :btnType="btnTypeCheckout"
                 @click="checkoutHandleClick"
@@ -35,6 +35,7 @@
                 class="ml-3"
                 :btnType="btnTypeClearAll"
                 :disabled="Object.keys(this.cartItems) <= 0"
+                :class="'border-none'"
                 @click="clearAllHandleClick"
             ></button-component>
         </div>
@@ -83,12 +84,14 @@ export default {
     data: function () {
         return {
             btnTypeCheckout: {
-                text: "Checkout",
+                text:
+                    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/></svg>' +
+                    "&emsp;Checkout",
                 type: "success",
             },
             btnTypeClearAll: {
                 text: "Clear All",
-                type: "danger",
+                type: "outline-danger",
             },
             checkoutComponentBody: CheckoutModalContent,
             checkoutComponentFooter: CheckoutModalFooterVue,
